70b70f33bc6e9eced8dc72c525fddcb4bcc3426c
ShreyasLadhe <shreyasladhe.23@gmail.com>
2026-02-21 21:59:51 +0530

    firestore name edgecase fix

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,7 +1,7 @@
 #FIRESTORE DB
 resource "google_firestore_database" "default_db" {
   project     = var.project_id
-  name        = "tf-code"
+  name        = "tf-code-${random_id.db_suffix.hex}"
   location_id = var.location_id
   type        = "FIRESTORE_NATIVE"
 }
